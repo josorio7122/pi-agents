372d7805a3bf5353f57902aa97fdb6fb1360ad16
Jose Osorio <josorio7122@gmail.com>
2026-04-10 17:25:32 -0500

    refactor: use reduce in resolveVariables instead of let reassignment

--- src/prompt/variables.ts
+++ src/prompt/variables.ts
@@ -1,7 +1,3 @@
 export function resolveVariables(template: string, variables: Readonly<Record<string, string>>) {
-  let result = template;
-  for (const [key, value] of Object.entries(variables)) {
-    result = result.replaceAll(`{{${key}}}`, value);
-  }
-  return result;
+  return Object.entries(variables).reduce((result, [key, value]) => result.replaceAll(`{{${key}}}`, value), template);
 }
